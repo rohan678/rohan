ce6aa5c07cbed69269a1a198e455f4e56f6bcdec
rohan678 <63397085+rohan678@users.noreply.github.com>
2020-08-21 16:39:51 +1000

    70%

---
+++ Assets/LoadAssets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+        
+{
+    public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blueObj = Instantiate(blueObj) as GameObject;
+        blueObj.transform.position = new Vector3(-2, 0, 0);
+        blueObj.transform.rotation = Quaternion.identity;
+        redObj = Instantiate(redObj) as GameObject;
+        redObj.transform.position = new Vector3(2, 0, 0);
+        redObj.transform.rotation = Quaternion.identity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
